9f199e6c7c216cca8b842b3a7d8169ad0b0e74ff
KobinaOtc <kobinaotchereadjoku@gmail.com>
2026-08-24 11:24:10 +0000

    Front end and back end communicating

---
+++ frontend-client/frontend-client/.next/dev/static/chunks/frontend-client_src_app_page_tsx_207iffu._.js
@@ -0,0 +1,8 @@
+(globalThis["TURBOPACK"] || (globalThis["TURBOPACK"] = [])).push([typeof document === "object" ? document.currentScript : undefined,
+"[project]/frontend-client/src/app/page.tsx [app-client] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+var e = new Error("Could not parse module '[project]/frontend-client/src/app/page.tsx', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+]);
